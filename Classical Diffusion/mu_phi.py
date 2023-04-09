c4f9b7d24d9feb3aa52cec4b1092bb7d2175839f
Shantanu Nitin Kor <77278554+s-h-a-n-k@users.noreply.github.com>
2023-04-09 23:53:05 +0530

    Add files via upload

--- Classical Diffusion/mu_phi.py
+++ Classical Diffusion/mu_phi.py
@@ -1,5 +1,4 @@
 import math
 
 
-def get_mu_gibbsboltzmann(alpha, c):return [alpha*(1 - 2 * i) + math.log(i / (1 - i)) for i in c]
-
+def get_mu_gibbsboltzmann(alpha, c):return list(map(lambda i:alpha*(1 - 2 * i) + math.log(i / (1 - i)), c));
